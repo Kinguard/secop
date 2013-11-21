0be573c3103767f6092010c743e78db420330d77
Tor Krill <tor@openproducts.se>
2013-11-21 16:13:24 +0100

    Minor changes
    * Add cmdline argument parsing
    * Add daemonizing
    * Add pidfile writing
    * Intercept sigint and terminate gracefully upon it
    
    Signed-off-by: Tor Krill <tor@openproducts.se>

--- secop.cpp
+++ secop.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <fstream>
 
 #include <libutils/Socket.h>
 #include <libutils/NetServer.h>
 #include <libutils/Logger.h>
 #include <libutils/FileUtils.h>
+#include <libutils/ArgParser.h>
 
 #include <unistd.h>
 #include <signal.h>
@@ -29,6 +31,7 @@ static void sighandler(int sig)
 	case SIGHUP:
 		break;
 	case SIGTERM:
+	case SIGINT:
 		logg << Logger::Info << "Got sigterm initiate shutdown"<<lend;
 		secop->ShutDown();
 		break;
@@ -43,6 +46,36 @@ int main(int argc, char** argv)
 
 	logg << Logger::Debug << "Kryptoapp starting up"<<lend;
 
+	ArgParser args;
+
+	args.AddOptions({
+						Option('d',"daemonize",Option::ArgNone,"0","Daemonize application"),
+						Option('p',"pidfile", Option::ArgRequired, "/var/run/secop.pid", "Path to pidfile"),
+						Option('u',"user", Option::ArgRequired, "secop", "User to run as"),
+						Option('g',"group", Option::ArgRequired, "secop", "Group to run as"),
+				});
+
+	if( ! args.Parse(argc,argv) )
+	{
+		logg << Logger::Error << "Invalid arguments"<<lend;
+		cerr << "Invalid arguments"<<endl;
+		return 1;
+	}
+
+	if( args["daemonize"] == "1" )
+	{
+		// Daemonize
+		if( daemon( 0, 0) < 0 )
+		{
+			logg << Logger::Error << "Failed to daemonize"<<lend;
+			return 1;
+		}
+		logg << Logger::Info << "Daemonized"<<lend;
+		ofstream of(args["pidfile"]);
+		of<<getpid()<<endl;
+		of.close();
+	}
+
 	unlink("/tmp/secop");
 	secop = SecopServerPtr( new SecopServer("/tmp/secop","/tmp/secop.db") );
 
@@ -53,6 +86,7 @@ int main(int argc, char** argv)
 	sigemptyset(&action.sa_mask);
 
 	sigaction(SIGTERM, &action, nullptr);
+	sigaction(SIGINT, &action, nullptr);
 	sigaction(SIGHUP, &action, nullptr);
 
 	secop->Run();
